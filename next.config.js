6d6c4ed7c73fb8be29d742afaf78bfd952b13443
Baaska Lhagbasuren <baaska1989@gmail.com>
2023-07-24 18:35:36 +0800

    aaaa

--- next.config.js
+++ next.config.js
@@ -1,21 +1,15 @@
-
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    newNextLinkBehavior: false,
-  },
-  swcMinify: true,
-  i18n: {
-    locales: ['jp', 'en'],
-    defaultLocale: 'jp',
-    localeDetection: false,
-  },
-  trailingSlash: true,
-  // output: 'export',
-  // images: {
-  //   loader: 'custom',
-  //   loaderFile: './app/image.ts',
-  // },
+    reactStrictMode: true,
+    experimental: {
+        newNextLinkBehavior: false,
+    },
+    swcMinify: true,
+    i18n: {
+        locales: ['jp', 'en'],
+        defaultLocale: 'jp',
+        localeDetection: false,
+    },
+    trailingSlash: true,
 }
 
 module.exports = nextConfig
