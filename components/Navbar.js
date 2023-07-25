cd942938ebcd68ca9cad12e815d79c288c7fb15c
Baaska Lhagbasuren <baaska1989@gmail.com>
2023-07-25 11:29:12 +0800

    some changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -16,172 +16,170 @@ export default function Navbar({ fixed }) {
 
   const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <>
-      <div style={{marginTop: "40px"}} className="sticky2 active:border-[3px] active:border-r-0 border-cyan-600 active:rounded mt-[10px] my-[3px] hidden lg:block">
-        <span style={{marginTop: "-10px"}}>🌐</span>
-        <select
-            onChange={changeLanguage}
-            defaultValue={locale}
-            className="text-white text-shadow-sm text-lg bg-transparent tracking-wide pd-[5]"
-        >
-          <option className="text-black" value="en">EN</option>
-          <option className="text-black" value="jp">JP</option>
-        </select>
-        {/*<Link href="#"> 🌐 言語 {t('app_title')} </Link>*/}
-      </div>
-      <nav
-        className="relative flex flex-wrap items-center justify-start py-3"
-        style={{ backgroundColor: "#0e1447" }}
-      >
-        <div className="container pl-8 mx-auto flex flex-wrap items-center justify-start ml-0">
-          <div className="w-full relative flex justify-start lg:w-auto lg:static lg:block">
-            <Link href="/">
-              <a className=" font-bold leading-relaxed inline-block py-2 whitespace-nowrap uppercase text-white">
-                <div>
-                  <img
-                    src="/assets/img/exe-logo.png"
-                    loading="lazy"
-                    className="w-28 px-2 transition duration-300 ease-linear align-middle"
-                  />
-                </div>
-              </a>
-            </Link>
-
-            <button
-              className="flex flex-grow justify-end items-center text-white font-light cursor-pointer text-4xl lg:hidden outline-none focus:outline-none"
-              type="button"
-              onClick={() => setNavbarOpen(!navbarOpen)}
-            >
-              <i className="fas fa-bars mr-8 lg:mr-0"></i>
-            </button>
-          </div>
-          <div
-            className={
-              "lg:flex flex-grow lg:flex-none items-center" +
-              (navbarOpen ? " flex" : " hidden")
-            }
-            id="example-navbar-danger"
+      <>
+        <div style={{marginTop: "40px"}} className="sticky2 active:border-[3px] active:border-r-0 border-cyan-600 active:rounded mt-[10px] my-[3px] hidden lg:block">
+          <span style={{marginTop: "-10px"}}>🌐</span>
+          <select
+              onChange={changeLanguage}
+              defaultValue={locale}
+              className="text-white text-shadow-sm text-lg bg-transparent tracking-wide pd-[5]"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto justify-start">
-              <li className="nav-item">
-                <Link href="/">
-                  <a className="px-3 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
-                    <span className="ml-2">{t.home}</span>
-                  </a>
-                </Link>
-              </li>
-              {/* dropdown */}
-              <div className="flex justify-start px-2">
-                <div>
-                  <div className="dropdown relative">
-                    <Link href="/about">
-                      <a className="px-1 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
-                        <span className="ml-2">{t.about}</span>
-                      </a>
-                    </Link>
-                    <ul
-                      className="dropdown-menu min-w-max absolute bg-darkblue text-base z-50 float-left py-2 list-none text-left shadow-lg mt-1 hidden bg-clip-padding border-none"
-                      aria-labelledby="dropdownMenuButton1tx"
-                    >
-                      <li>
-                        <Link href="/greetings">
-                          <a className="dropdown-item px-12 text-sm py-2 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 bg-darkblue">
-                            社長メッセージ
-                          </a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/corporate">
-                          <a className="dropdown-item px-12 text-sm py-2 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100">
-                            会社概要
-                          </a>
-                        </Link>
-                      </li>
-                    </ul>
+            <option className="text-black" value="en">EN</option>
+            <option className="text-black" value="jp">JP</option>
+          </select>
+          {/*<Link href="#"> 🌐 言語 {t('app_title')} </Link>*/}
+        </div>
+        <nav
+            className="relative flex flex-wrap items-center justify-start py-3"
+            style={{ backgroundColor: "#0e1447" }}
+        >
+          <div className="container pl-8 mx-auto flex flex-wrap items-center justify-start ml-0">
+            <div className="w-full relative flex justify-start lg:w-auto lg:static lg:block">
+              <Link href="/">
+                <a className=" font-bold leading-relaxed inline-block py-2 whitespace-nowrap uppercase text-white">
+                  <div>
+                    <img
+                        src="/assets/img/exe-logo.png"
+                        loading="lazy"
+                        className="w-28 px-2 transition duration-300 ease-linear align-middle"
+                    />
+                  </div>
+                </a>
+              </Link>
+
+              <button
+                  className="flex flex-grow justify-end items-center text-white font-light cursor-pointer text-4xl lg:hidden outline-none focus:outline-none"
+                  type="button"
+                  onClick={() => setNavbarOpen(!navbarOpen)}
+              >
+                <i className="fas fa-bars mr-8 lg:mr-0"></i>
+              </button>
+            </div>
+            <div
+                className={
+                    "lg:flex flex-grow lg:flex-none items-center" +
+                    (navbarOpen ? " flex" : " hidden")
+                }
+                id="example-navbar-danger"
+            >
+              <ul className="flex flex-col lg:flex-row list-none lg:ml-auto justify-start">
+                <li className="nav-item">
+                  <Link href="/">
+                    <a className="px-3 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
+                      <span className="ml-2">{t.home}</span>
+                    </a>
+                  </Link>
+                </li>
+                {/* dropdown */}
+                <div className="flex justify-start px-2">
+                  <div>
+                    <div className="dropdown relative">
+                      <Link href="/about">
+                        <a className="px-1 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
+                          <span className="ml-2">{t.about}</span>
+                        </a>
+                      </Link>
+                      <ul
+                          className="dropdown-menu min-w-max absolute bg-darkblue text-base z-50 float-left py-2 list-none text-left shadow-lg mt-1 hidden bg-clip-padding border-none"
+                          aria-labelledby="dropdownMenuButton1tx"
+                      >
+                        <li>
+                          <Link href="/greetings">
+                            <a className="dropdown-item px-12 text-sm py-2 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 bg-darkblue">
+                              社長メッセージ
+                            </a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/corporate">
+                            <a className="dropdown-item px-12 text-sm py-2 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100">
+                              会社概要
+                            </a>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              {/* dropdown */}
+                {/* dropdown */}
 
-              <div className="flex justify-center">
-                <li className="nav-item">
-                  <div className="flex justify-center">
-                    <div className="dropdown -ml-8 md:ml-0">
+                <div className="flex justify-center">
+                  <li className="nav-item">
+                    <div className="flex justify-center">
+                      <div className="dropdown -ml-8 md:ml-0">
                       <span className="flex items-center md:px-6 pt-2.5 pb-2 text-sm font-medium uppercase leading-normal text-white -mt-0.5">
                         {t.overseasProjects}
                       </span>
-                      <div className="dropdown-content text-center">
-                        <a
-                          className="block w-full bg-transparent py-1 px-4 text-sm  text-white border"
-                          href="/myanmar"
-                        >
-                          ミャンマー
-                        </a>
-                        <a
-                          className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
-                          href="/mongolia"
-                        >
-                          モンゴル
-                        </a>
+                        <div className="dropdown-content text-left text-white">
+                          <Link
+                              className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
+                              href="/myanmar"
+                          >
+                            {t.myanmar}
+                          </Link> <br />
+                          <Link style={{color: "#fff", padding: 10}}
+                              className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
+                              href="/mongolia"
+                          >
+                            {t.mongolia}
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </li>
+                </div>
+
+                <li className="nav-item">
+                  <Link href="/domestic">
+                    <a className="px-3 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
+                      <span className="ml-2">{t.domesticProjects}</span>
+                    </a>
+                  </Link>
                 </li>
-              </div>
 
-              <li className="nav-item">
-                <Link href="/domestic">
-                  <a className="px-3 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
-                    <span className="ml-2">{t.domesticProjects}</span>
-                  </a>
-                </Link>
-              </li>
-
-              <div className="flex justify-center">
-                <li className="nav-item1">
-                  <div className="flex justify-center">
-                    <div className="dropdown text-white">
+                <div className="flex justify-center">
+                  <li className="nav-item1">
+                    <div className="flex justify-center">
+                      <div className="dropdown text-white">
                       <span className="flex items-center bg-darkblue lg:px-6 px-4 pt-2.5 pb-2 text-sm font-medium uppercase leading-normal text-white -mt-0.5">
                         {t.internship}
                       </span>
-                      <div className="dropdown-content text-center">
-                        <a
-                          className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
-                          href="/intership"
-                          data-te-dropdown-item-ref
-                        >
-                          {t.internship}
-                        </a>
-                        <a
-                          className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
-                          href="/recruit"
-                          data-te-dropdown-item-ref
-                        >
-                          リクルート
-                        </a>
+                        <div className="dropdown-content text-center">
+                          <Link
+                              className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
+                              href="/intership"
+                              data-te-dropdown-item-ref
+                          >
+                            {t.internship}
+                          </Link>
+                          <Link
+                              className="block w-full bg-transparent py-1 px-4 text-sm text-white border"
+                              href="/recruit"
+                              data-te-dropdown-item-ref
+                          >
+                            {t.recruit}
+                          </Link>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              </div>
+                  </li>
+                </div>
 
-              <li className="nav-item">
-                <Link href="/contact">
-                  <a className="px-2 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
-                    <span className="ml-2">{t.contact}</span>
-                    {/*<span className="ml-2">お問い合わせ</span>*/}
-                  </a>
-                </Link>
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <Link href="/contact">
+                    <a className="px-2 py-2 flex items-center text-sm uppercase leading-snug text-white hover:text-sky-300">
+                      <span className="ml-2">{t.contact}</span>
+                      {/*<span className="ml-2">お問い合わせ</span>*/}
+                    </a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
           </div>
 
-        </div>
-
-      </nav>
-
-
-    </>
+        </nav>
+      </>
   );
 }
--- components/Sharedhouse.js
+++ components/Sharedhouse.js
@@ -1,42 +1,47 @@
 import React from "react";
 import Link from "next/link";
+import en from '../locales/en';
+import jp from '../locales/jp';
+import { useRouter } from "next/router";
 
 const Sharedhouse = () => {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === 'jp' ? jp : en;
   return (
-    <div className="py-8 max-w-[1200px] mx-auto">
-      <div className="flex justify-center">
-        <div className="grid md:grid-cols-2">
-          <div className="p-4 md:px-8">
-            <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 text-center md:text-left">
-              モンゴルプロジェクト
-            </h2>
-            <p className="my-4 text-lg font-medium text-gray-500 text-justify">
-              2010年に日本初の投資事業会社Sanaa EXE
-              LLCを設立し、不動産投資事業、金融事業、教育事業を展開しています。エクセプラザ、Olonlog」スクールを運営しています。
-            </p>
-            <div className="flex justify-center md:justify-start align-text-bottom">
-              <Link href="/mongolia">
-                <button
-                  className="px-14 py-3 bg-darkblue text-gray-400 font-extrabold text-base leading-snug uppercase"
-                  role="button"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                >
-                  詳細はこちら
-                </button>
-              </Link>
+      <div className="py-8 max-w-[1200px] mx-auto">
+        <div className="flex justify-center">
+          <div className="grid md:grid-cols-2">
+            <div className="p-4 md:px-8">
+              <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 text-center md:text-left">
+                {t.sharedhouse.title}
+              </h2>
+              <p className="my-4 text-lg font-medium text-gray-500 text-justify">
+                {t.sharedhouse.text}
+              </p>
+              <div className="flex justify-center md:justify-start align-text-bottom">
+                <Link href="/mongolia">
+                  <button
+                      className="px-14 py-3 bg-darkblue text-gray-400 font-extrabold text-base leading-snug uppercase"
+                      role="button"
+                      data-mdb-ripple="true"
+                      data-mdb-ripple-color="light"
+                  >
+                    {t.aboutExe.button}
+                  </button>
+                </Link>
+              </div>
             </div>
-          </div>
-          <div className="md:px-4 row-start-1 md:row-span-1">
-            <img
-              src="/assets/img/exe website mongolia.jpg"
-              loading="lazy"
-              className="w-75% h-auto align-middle mx-auto bg-center bg-cover"
-            />
+            <div className="md:px-4 row-start-1 md:row-span-1">
+              <img
+                  src="/assets/img/exe website mongolia.jpg"
+                  loading="lazy"
+                  className="w-75% h-auto align-middle mx-auto bg-center bg-cover"
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
